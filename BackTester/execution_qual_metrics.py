f7acc80f33177a9db1168f5ae903b8ee823aaa66
xzhang1988 <xzhang19880107@gmail.com>
2017-11-12 19:03:41 -0500

    Create execution_qual_metrics.py

---
+++ BackTester/execution_qual_metrics.py
@@ -0,0 +1,2 @@
+% a library for the metrics of order execution strategy performance, ex-ante and ad-hoc trading cost and market impact, 
+% volume weighted order price/spot mid-price ratio, half life of an order, etc.
